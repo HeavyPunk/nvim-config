9315c9364d1c772c4eeb084d3444d620f6fc6e0a
HeavyPunk <kirillkuchma8@gmail.com>
2025-09-28 18:59:54 +0500

    draft: update lsp configurations

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -47,107 +47,217 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-local lspconfig = require("lspconfig")
+-- local lspconfig = require("lspconfig")
+-- local lspconfig = vim.lsp.config
 
-lspconfig.lua_ls.setup({
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  on_init = M.on_init,
+local lspconfig = function (server, opts)
+  vim.lsp.config(server, opts)
+  vim.lsp.enable(server)
+end
 
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          vim.fn.expand "$VIMRUNTIME/lua",
-          vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-          vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-          vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-          "${3rd}/luv/library",
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
-})
+-- lspconfig("lua_ls", {
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   on_init = M.on_init,
+--
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = { "vim" },
+--       },
+--       workspace = {
+--         library = {
+--           vim.fn.expand "$VIMRUNTIME/lua",
+--           vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
+--           vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+--           vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+--           "${3rd}/luv/library",
+--         },
+--         maxPreload = 100000,
+--         preloadFileSize = 10000,
+--       },
+--     },
+--   },
+-- })
 
-lspconfig.csharp_ls.setup({
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  handlers = {
-    ["textDocument/definition"] = require('csharpls_extended').handler,
-    ["textDocument/typeDefinition"] = require('csharpls_extended').handler,
-  },
-  filetypes = {"cs"},
-  cmd = {"csharp-ls"},
-  init_options = {
-    AutomaticWorkspaceInit = true
-  }
-})
+-- lspconfig("csharp_ls", {
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   handlers = {
+--     ["textDocument/definition"] = require('csharpls_extended').handler,
+--     ["textDocument/typeDefinition"] = require('csharpls_extended').handler,
+--   },
+--   filetypes = {"cs"},
+--   cmd = {"csharp-ls"},
+--   init_options = {
+--     AutomaticWorkspaceInit = true
+--   }
+-- })
 
-lspconfig.rust_analyzer.setup({
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+lspconfig("rust_analyzer", {
+  -- on_init = M.on_init,
+  -- on_attach = M.on_attach,
+  -- capabilities = M.capabilities,
   filetypes = {"rust"},
-  cargo = {
-    allFeature = true
-  }
-})
 
-lspconfig.clangd.setup({
-  on_attach = M.on_attach,
-  on_init = M.on_init,
-  capabilities = M.capabilities,
-  filetypes = {"c", "h", "cpp", "hpp"},
-  root_dir = function() return vim.fn.getcwd() end,
-  settings = {
-    ["clangd"] = {
-      cmd = {"clangd", "--background-index", "--clang-tidy", "--log=verbose"},
-      init_options = {
-        fallback_flags = {"-std=c++17"}
-      }
-    }
-  }
+  root_dir = function(fname)
+    print("root_dir called with:", fname)
+    local cwd = vim.fn.getcwd()
+    print("Returning cwd as root_dir:", cwd)
+    return cwd
+  end,
+  cmd = {"rust-analyzer"},
+  -- settings = {
+  --   ["rust-analyzer"] = {   
+  --     cargo = {
+  --       allFeatures = true
+  --     }
+  --   },
+  -- }
 })
+--
+-- lspconfig.lua_ls.setup({
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   on_init = M.on_init,
+--
+--   settings = {
+--     Lua = {
+--       diagnostics = {
+--         globals = { "vim" },
+--       },
+--       workspace = {
+--         library = {
+--           vim.fn.expand "$VIMRUNTIME/lua",
+--           vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
+--           vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+--           vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+--           "${3rd}/luv/library",
+--         },
+--         maxPreload = 100000,
+--         preloadFileSize = 10000,
+--       },
+--     },
+--   },
+-- })
 
-lspconfig.gopls.setup({
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  filetypes = {"go", "golang"},
-  root_dir = function() return vim.fn.getcwd() end,
-  settings = {
-    ["gopls"] = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
-})
+-- lspconfig.csharp_ls.setup({
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   handlers = {
+--     ["textDocument/definition"] = require('csharpls_extended').handler,
+--     ["textDocument/typeDefinition"] = require('csharpls_extended').handler,
+--   },
+--   filetypes = {"cs"},
+--   cmd = {"csharp-ls"},
+--   init_options = {
+--     AutomaticWorkspaceInit = true
+--   }
+-- })
 
-lspconfig.kotlin_language_server.setup({
-  on_init = M.on_init,
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
-  filetypes = {"kotlin"},
-  -- root_dir = function () return vim.fn.getcwd() end,
-  -- init_options = {
-  --  storagePath = vim.fn.resolve(vim.fn.stdpath("cache") .. "/kotlin_language_server")
-  --} -- https://github.com/neovim/nvim-lspconfig/issues/3239
-})
+-- lspconfig.rust_analyzer.setup({
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   filetypes = {"rust"},
+--   cargo = {
+--     allFeature = true
+--   }
+-- })
 
-lspconfig.ts_ls.setup({
+
+-- lspconfig("clangd", {
+--   on_attach = M.on_attach,
+--   on_init = M.on_init,
+--   capabilities = M.capabilities,
+--   filetypes = {"c", "h", "cpp", "hpp"},
+--   root_dir = function() return vim.fn.getcwd() end,
+--   settings = {
+--     ["clangd"] = {
+--       cmd = {"clangd", "--background-index", "--clang-tidy", "--log=verbose"},
+--       init_options = {
+--         fallback_flags = {"-std=c++17"}
+--       }
+--     }
+--   }
+-- })
+
+-- lspconfig.clangd.setup({
+--   on_attach = M.on_attach,
+--   on_init = M.on_init,
+--   capabilities = M.capabilities,
+--   filetypes = {"c", "h", "cpp", "hpp"},
+--   root_dir = function() return vim.fn.getcwd() end,
+--   settings = {
+--     ["clangd"] = {
+--       cmd = {"clangd", "--background-index", "--clang-tidy", "--log=verbose"},
+--       init_options = {
+--         fallback_flags = {"-std=c++17"}
+--       }
+--     }
+--   }
+-- })
+
+-- lspconfig("gopls", {
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   filetypes = {"go", "golang"},
+--   root_dir = function() return vim.fn.getcwd() end,
+--   settings = {
+--     ["gopls"] = {
+--       analyses = {
+--         unusedparams = true,
+--       },
+--       staticcheck = true,
+--       gofumpt = true,
+--     },
+--   },
+-- })
+
+
+-- lspconfig.gopls.setup({
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   filetypes = {"go", "golang"},
+--   root_dir = function() return vim.fn.getcwd() end,
+--   settings = {
+--     ["gopls"] = {
+--       analyses = {
+--         unusedparams = true,
+--       },
+--       staticcheck = true,
+--       gofumpt = true,
+--     },
+--   },
+-- })
+
+-- lspconfig.kotlin_language_server.setup({
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   filetypes = {"kotlin"},
+--   -- root_dir = function () return vim.fn.getcwd() end,
+--   -- init_options = {
+--   --  storagePath = vim.fn.resolve(vim.fn.stdpath("cache") .. "/kotlin_language_server")
+--   --} -- https://github.com/neovim/nvim-lspconfig/issues/3239
+-- })
+lspconfig("ts_ls", {
   on_init = M.on_init,
   on_attach = M.on_attach,
   capabilities = M.capabilities,
   filetypes = {"typescript", "typescriptreact", "javascript", "javascriptreact"},
   root_dir = function() return vim.fn.getcwd() end
 })
+
+-- lspconfig.ts_ls.setup({
+--   on_init = M.on_init,
+--   on_attach = M.on_attach,
+--   capabilities = M.capabilities,
+--   filetypes = {"typescript", "typescriptreact", "javascript", "javascriptreact"},
+--   root_dir = function() return vim.fn.getcwd() end
+-- })
 
