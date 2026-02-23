30da57392f718cad1a4ef56080ef81446b83cc74
HeavyPunk <kirillkuchma8@gmail.com>
2026-02-23 19:13:40 +0500

    feat: add mcp hub

--- lua/plugins/codecompanion.nvim.lua
+++ lua/plugins/codecompanion.nvim.lua
@@ -3,6 +3,7 @@ return {
   dependencies = {
     { "nvim-lua/plenary.nvim", branch = "master" },
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     -- {
     --   "milanglacier/minuet-ai.nvim",
@@ -13,6 +14,74 @@ return {
     -- }
   },
   opts = {
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          -- MCP Tools
+          make_tools = true,              -- Make individual tools (@server__tool) and server groups (@server) from MCP servers
+          show_server_tools_in_chat = true, -- Show individual tools in chat completion (when make_tools=true)
+          add_mcp_prefix_to_tool_names = false, -- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
+          show_result_in_chat = true,      -- Show tool results directly in chat buffer
+          format_tool = nil,               -- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
+          -- MCP Resources
+          make_vars = true,                -- Convert MCP resources to #variables for prompts
+          -- MCP Prompts
+          make_slash_commands = true,      -- Add MCP prompts as /slash commands
+        }
+      }
+    },
+    prompt_library = {
+      ["Edit<->Test workflow"] = {
+        strategy = "workflow",
+        description = "Use a workflow to repeatedly edit then test code",
+        opts = {
+          index = 5,
+          is_default = true,
+          short_name = "et",
+        },
+        prompts = {
+          {
+            {
+              name = "Setup Test",
+              role = "user",
+              opts = { auto_submit = false },
+              content = function()
+                local approvals = require("codecompanion.interactions.chat.tools.approvals")
+                approvals:toggle_yolo_mode()
+                return
+[[### Instructions
+Your instructions here
+
+### Steps to Follow
+
+You are required to write code following the instructions provided above and test the correctness by running the designated test suite. Follow these steps exactly:
+
+1. Update the code in #{buffer} using the @{insert_edit_into_file} tool
+2. Then use the @{cmd_runner} tool to run the test suite with `<test_cmd>` (do this after you have updated the code)
+3. Make sure you trigger both tools in the same response
+
+We'll repeat this cycle until the tests pass. Ensure no deviations from these steps.]]
+              end,
+            },
+          },
+          {
+            {
+              name = "Repeat On Failure",
+              role = "user",
+              opts = { auto_submit = true },
+              condition = function(chat)
+                return chat.tools.tool and chat.tools.tool.name == "cmd_runner"
+              end,
+              repeat_until = function(chat)
+                return chat.tool_registry.flags.testing == true
+              end,
+              content = "The tests have failed. Can you edit the buffer and run the test suite again?",
+            },
+          },
+        }
+      }
+    },
     display = {
       diff = {
         enabled = true,
@@ -21,71 +90,6 @@ return {
         opts = { "internal", "filler", "closeoff", "algorithm:patience", "followwrap", "linematch:120" },
         provider = "default", -- default|mini_diff
       },
-    },
-    adapters = {
-      http = {
-        kontur_llm = function()
-          return require("codecompanion.adapters").extend("openai_compatible", {
-            env = {
-              url = "https://srs-litellm.kontur.host",
-              api_key = vim.env.CLAUDE_API_KEY,
-              chat_url = "/v1/chat/completions",
-              models_endpoint = "/v1/models"
-            },
-            schema = {
-              temperature = {
-                order = 2,
-                mapping = "parameters",
-                type = "number",
-                optional = true,
-                default = 0.8,
-                validate = function (n)
-                  return n >= 0 and n <= 2, "Must be between 0 and 2"
-                end
-              },
-              max_completion_tokens = {
-                order = 3,
-                mapping = "parameters",
-                type = "integer",
-                optional = true,
-                default = nil,
-                desc = "An upper bound for the number of tokens that can be generated for a completion.",
-                validate = function(n)
-                  return n > 0, "Must be greater than 0"
-                end,
-              },
-              stop = {
-                order = 4,
-                mapping = "parameters",
-                type = "string",
-                optional = true,
-                default = nil,
-                desc = "Sets the stop sequences to use. When this pattern is encountered the LLM will stop generating text and return. Multiple stop patterns may be set by specifying multiple separate stop parameters in a modelfile.",
-                validate = function(s)
-                  return s:len() > 0, "Cannot be an empty string"
-                end,
-              },
-              logit_bias = {
-                order = 5,
-                mapping = "parameters",
-                type = "map",
-                optional = true,
-                default = nil,
-                desc = "Modify the likelihood of specified tokens appearing in the completion. Maps tokens (specified by their token ID) to an associated bias value from -100 to 100. Use https://platform.openai.com/tokenizer to find token IDs.",
-                subtype_key = {
-                  type = "integer",
-                },
-                subtype = {
-                  type = "integer",
-                  validate = function(n)
-                    return n >= -100 and n <= 100, "Must be between -100 and 100"
-                  end,
-                },
-              }
-            }
-          })
-        end,
-      }
     },
     strategies = {
       chat = {
@@ -114,45 +118,14 @@ return {
         adapter = "copilot",
         model = "claude-sonnet-4-20250514"
       },
-
-      -- chat = {
-      --   opts = {
-      --     completion_provider = "cmp",
-      --   },
-      --   roles = {
-      --     ---@type string|fun(adapter: CodeCompanion.Adapter): string
-      --     llm = function (adapter)
-      --       return "KonturLLM (" .. adapter.formatted_name .. ")"
-      --     end,
-      --     user = "Me",
-      --   },
-      --   adapter = {
-      --     name = "kontur_llm",
-      --     model = "claude_opus_4",
-      --   },
-      -- },
-      -- inline = {
-      --   adapter = {
-      --     name = "kontur_llm",
-      --     model = "gpt4_o_mini",
-      --   },
-      --   keymaps = {
-      --     accept_change = {
-      --       modes = { n = "ct" },
-      --       description = "Accept the suggested change",
-      --     },
-      --     reject_change = {
-      --       modes = { n = "co" },
-      --       description = "Reject the suggested change",
-      --     }
-      --   }
-      -- },
-      -- agent = {
-      --   adapter = {
-      --     name = "kontur_llm",
-      --     model = "gpt4_o_mini",
-      --   }
-      -- }
     },
+    interactions = {
+      chat = {
+        adapter = {
+          name = "copilot",
+          model = "claude-sonnet-4-20250514"
+        }
+      }
+    }
   }
 }
